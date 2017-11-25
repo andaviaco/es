456188249b49d3b38707c9f639829d3d6725d968
Andrés Ávila <andaviaco@gmail.com>
2017-11-24 21:19:59 -0600

    optimize Ackley and Rastrigin functions

--- src/main.py
+++ src/main.py
@@ -3,7 +3,19 @@ import lib
 
 def main():
     es_sphere = ES(100, 100, lib.sphere, lb=[-5, -5], ub=[5, 5])
+    es_ackley = ES(100, 100, lib.ackley, lb=[-20, -20], ub=[20, 20])
+    es_rastrigin = ES(100, 100, lib.rastrigin, lb=[-5, -5], ub=[5, 5])
+
     best = es_sphere.optimize()
+    print('Sphere')
+    print(f'MIN: {best}')
+
+    best = es_ackley.optimize()
+    print('Ackley')
+    print(f'MIN: {best}')
+
+    best = es_rastrigin.optimize()
+    print('Rastrigin')
     print(f'MIN: {best}')
 
 if __name__ == '__main__':
